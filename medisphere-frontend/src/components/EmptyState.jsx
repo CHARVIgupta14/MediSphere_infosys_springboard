7c91ea49d8ee274f507ec5584d36b3e191269335
Charvi Gupta <guptacharvi14@gmail.com>
2026-09-08 18:03:06 +0530

    Remove

--- medisphere-frontend/src/components/EmptyState.jsx
+++
@@ -1,9 +0,0 @@
-import './EmptyState.css'
-
-export default function EmptyState({ message = 'No data available.' }) {
-  return (
-    <div className="empty-state">
-      <p>{message}</p>
-    </div>
-  )
-}
--- medisphere-frontend/src/services/fhirService.js
+++
@@ -1,9 +0,0 @@
-import api from './api'
-
-export const getFhirPatients = () => api.get('/api/fhir/patients')
-
-export const getFhirPatientTwin = (patientId) =>
-  api.get(`/api/fhir/patients/${patientId}/twin`)
-
-export const importFhirPatient = (patientId) =>
-  api.post(`/api/fhir/patients/${patientId}/import`)
